fe7f562e030dc2bb29493c95e14e0ac22a2c7df1
Yahir Alberto <136031489+yahirdev13@users.noreply.github.com>
2024-10-02 12:16:42 -0600

    cambio en home

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -2,37 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/widgets/content_column.dart';
 
 class Home extends StatelessWidget {
-const Home({ super.key });
+  const Home({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       body: const Padding(
         padding: EdgeInsets.all(16.0),
         child: Row(
           children: [
             ContentColumn(
-              title: 'Primer Columna', 
+              title: 'Primer Columna',
               description: 'Primer Parrafo',
             ),
             ContentColumn(
-              title: 'Segunda Columna', 
+              title: 'Segunda Columna',
               description: 'Segundo Parrafo',
             ),
             ContentColumn(
-              title: 'Tercer Columna', 
+              title: 'Tercer Columna',
               description: 'Tercer Parrafo',
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.blue, foregroundColor: Colors.amber,
-        onPressed: ()=> print('Hola'), 
-        child: Icon(Icons.home),
+        backgroundColor: const Color.fromARGB(255, 4, 196, 116),
+        foregroundColor: const Color.fromARGB(255, 255, 132, 61),
+        onPressed: () => Navigator.pushNamed(context, '/top'),
+        child: Icon(Icons.arrow_forward),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
-
